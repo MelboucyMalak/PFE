6b2806bfec55286c53d84858f721970b3492f437
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-03-23 15:27:56 +0100

    finish later

---
+++ crop_backend/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from user import views
+from crop import urls as crop_urls
+
+urlpatterns = [
+    path('userLogin/', ),
+]
--- crop_backend/user/views.py
+++ crop_backend/user/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.contrib.auth import  login, authenticate,logout
+
 
 # Create your views here.
+def log_in(request):
+    login(request)
+    return HttpResponse('log in page ')
+
+def log_out(request):
+    logout(request)
+    return redirect('home')
