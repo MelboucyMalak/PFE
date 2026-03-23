db38ebb1db7ffe7e57aff49a82375d8dc63f8b6a
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-03-23 12:52:03 +0100

    fix errors

--- crop_backend/crop/models.py
+++ crop_backend/crop/models.py
@@ -1,18 +1,20 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
-from django.db.models import Q
+from django.db.models import Q , CheckConstraint
+
 
 
 # Create your models here.
 class Crop(models.Model):
     name = models.CharField(max_length=50)
-    PH= models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(14.0)]) #PH BETWEEN 0-14
-    P= models.FloatField()
-    K= models.FloatField()
-    N= models.FloatField()
+    ph= models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(14.0)]) #PH BETWEEN 0-14
+    p= models.FloatField()
+    k= models.FloatField()
+    n= models.FloatField()
     def __str__(self):
-        return self.crop
-    class Meta:
+        return self.name
+    '''class Meta:
         constraints = [
-            models.CheckConstraint(check=Q(ph__gte=0.0) & Q(ph__lte=14.0),name='ph_between_0_and_14')
-        ] # gte: greater or =  //  lte: less or =
+            models.CheckConstraint(check=Q(ph__gte=0.0) & Q(ph__lte=14.0),
+                                   name='ph_between_0_and_14')
+        ] # gte: greater or =  //  lte: less or ='''
