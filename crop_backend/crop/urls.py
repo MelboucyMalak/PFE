77aafe11a830249abda70137587c956659daf71f
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-03-24 22:59:32 +0100

    add api

--- crop_backend/crop/urls.py
+++ crop_backend/crop/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from crop import  api
 
 urlpatterns = [
-    path('api/crop_list',api.crop_list),
+    path('api/crop_list',api.crop_list_api),
     #path('a',views.crop_detail),
 ]
