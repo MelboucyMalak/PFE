24d26423d30da11409df138cf1fd7f474fbf99bb
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-03-24 18:25:14 +0100

    add recommendation app to installed apps

--- crop_backend/_myProject/settings.py
+++ crop_backend/_myProject/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'user.apps.UserConfig',
     'crop.apps.CropConfig'
+    'recommendations.apps.RecommendationsConfig',
 ]
 
 MIDDLEWARE = [
