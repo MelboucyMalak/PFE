1a48ec62a382a16f30942b961ebb71c7cbc00957
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-03-24 22:55:53 +0100

    add serializer (recommendation+plan) to recommendation app

---
+++ crop_backend/recommendation/serializers.py
@@ -0,0 +1,9 @@
+
+
+class RecommendationSerializer(serializers.ModelSerializer):
+    model=Recommendation
+    fields = '__all__'
+
+class PlanSerializer(serializers.ModelSerializer):
+    model=Plan
+    fields = '__all__'
