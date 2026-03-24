96ac4a7299d120b13bfdf78eb1ded3298d22921a
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-03-24 09:19:16 +0100

    fix the url

--- crop_backend/crop/urls.py
+++ crop_backend/crop/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from crop import views
 
 urlpatterns = [
-  path('cropList/', views.crops_list, name='cropList' ),
-  path('cropList/<str:name>/',views.crop,name='crop'),
+  path('', views.crops_list, name='cropList' ),
+  path('cropProtocole/',views.crop,name='crop'),
 ]
