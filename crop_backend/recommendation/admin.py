1d4880da3b6fa680913e1347a91d27381ab02e3b
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-03-24 18:40:49 +0100

    add plan,recommendation to admin site

--- crop_backend/recommendation/admin.py
+++ crop_backend/recommendation/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from recommendation.models import Recommendation , Plan
 
-# Register your models here.
+admin.site.register(Recommendation)
+admin.site.register(Plan)
+
