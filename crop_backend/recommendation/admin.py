306d10a2b3b568eb0ab81f956f2725b435d3fd46
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-04-03 15:57:08 +0100

    register table recommendationSession + CropRecommendation

--- crop_backend/recommendation/admin.py
+++ crop_backend/recommendation/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 """from recommendation.models import Recommendation , Plan
 
-admin.site.register(Recommendation)
-admin.site.register(Plan)"""
+admin.site.register(RecommendationSession)
+admin.site.register(CropRecommendation)
 
