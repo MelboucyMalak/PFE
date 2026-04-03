2c9945cb6e0ef9a8a8c2fea2bf264d51724ecb15
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-04-03 15:57:27 +0100

    import the needed models

--- crop_backend/recommendation/admin.py
+++ crop_backend/recommendation/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-"""from recommendation.models import Recommendation , Plan
+from recommendation.models import RecommendationSession , CropRecommendation
 
 admin.site.register(RecommendationSession)
 admin.site.register(CropRecommendation)
