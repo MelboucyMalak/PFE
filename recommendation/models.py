c845c3951c690a4e3412c37d2a700bac09c9b693
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-04-10 18:03:24 +0100

    remove field user from recommendation move model history to user app

--- recommendation/models.py
+++ recommendation/models.py
@@ -9,7 +9,6 @@ from crop.models import Crop
 
 # 𝗖𝗟𝗔𝗦𝗦 𝗥𝗘𝗖𝗢𝗠𝗠𝗘𝗡𝗗𝗔𝗧𝗜𝗢𝗡 𝗦𝗘𝗦𝗦𝗜𝗢𝗡
 class RecommendationSession(models.Model):
-    user=models.ForeignKey(User, on_delete=models.CASCADE)
     date=models.DateTimeField(default=timezone.now)
     lat=models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(90)])
     lon=models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
