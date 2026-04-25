8375087b32c9b44b64277a5572f7ca5ed2111384
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-04-25 20:27:17 +0100

    describe change

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from recommendation import views
 urlpatterns = [
-    path("api/recommenadation",views.recommendation_api_view),
-    path("api/crop-recomendation",views.croplist_api_view),
+    path("api/recommendation",views.recommendation_api_view),
+    path("api/crop-recommendation",views.croplist_api_view),
     path("api/recommendation/favorite",views.favorite_api_view),
 ]
