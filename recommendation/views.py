dd1b7085adb73970344adde9cdb6abf0c435ddd7
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-04-27 17:58:49 +0100

    Resolved merge conflicts

--- recommendation/views.py
+++ recommendation/views.py
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 from django.http import JsonResponse
-=======
-from venv import create
-
->>>>>>> 6f7eb695a30bb2034d83514c02d090de670d6f60
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
 from _myProject.Errors.responses import error_response
 from .ExternalApiService import get_weather_data, is_inside_algeria
 from .models import RecommendationSession
@@ -29,13 +23,8 @@ def recommendation_api_view(request):
     lat = request.data.get('lat')
     lon = request.data.get('lon')
     if lat==None or lon==None :
-<<<<<<< HEAD
         return error_response("MISSING_COORDINATES")
-    recommendations = creatRecommendation(request)
-=======
-        return Response({'error':'Latitude or longitude is required'},status=status.HTTP_400_BAD_REQUEST)
     recommendations = createRecommendation(request)
->>>>>>> 6f7eb695a30bb2034d83514c02d090de670d6f60
     data = RecommendationSerializer(recommendations, many=False).data
     return Response({'recommendations': data},status=status.HTTP_200_OK)
 
