57e1cea72bd69f4186d74077bc7840722a0fb1b6
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-04-29 14:24:02 +0100

    trying to fix initEE

--- recommendation/apps.py
+++ recommendation/apps.py
@@ -1,9 +1,10 @@
 from django.apps import AppConfig
 
-from recommendation.ExternalApiService import initEE
+
 
 
 class RecommendationConfig(AppConfig):
     name = 'recommendation'
     def ready(self):
+        from.ExternalApiService import initEE
         initEE()
