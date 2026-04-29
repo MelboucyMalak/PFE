d94023f90c3502386d4e3ab92f354503df5ec04b
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-04-29 15:03:35 +0100

    sqllite3 in local and postgressdb in render

--- _myProject/settings.py
+++ _myProject/settings.py
@@ -78,12 +78,17 @@ WSGI_APPLICATION = '_myProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
