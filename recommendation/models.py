da6bf07a65721b92f6c0e1e6e50e6f46bc7c9dce
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-05-01 09:52:29 +0100

    .

--- recommendation/models.py
+++ recommendation/models.py
@@ -1,5 +1,4 @@
 from django.core.validators import MinValueValidator, MaxValueValidator
-from django.db.models import Q
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.db import models
