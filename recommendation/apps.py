f7fa881631c718d064df461c96331be924aec622
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-05-04 11:35:35 +0100

    fix ee error

--- recommendation/apps.py
+++ recommendation/apps.py
@@ -6,8 +6,5 @@ from django.apps import AppConfig
 class RecommendationConfig(AppConfig):
     name = 'recommendation'
     def ready(self):
-        import os
-        if os.environ.get('RUN_MAIN') != 'true':
-            return
         from .ExternalApiService import initEE
         initEE()
