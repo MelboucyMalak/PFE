f6406dabe6b1183c4a9357e680e554d0d60bad6c
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-05-04 14:08:57 +0100

    add sqlite fallback

--- _myProject/settings.py
+++ _myProject/settings.py
@@ -76,11 +76,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = '_myProject.wsgi.application'
 
-
 DATABASE_URL = os.environ.get('DATABASE_URL')
-DATABASES = {
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
         'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
     }
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 AUTH_PASSWORD_VALIDATORS = [
