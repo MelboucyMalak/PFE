cb66e4ceb79e5c3575c391be75f6541c289d1f92
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-05-20 09:46:53 +0100

    uncomment the db

--- _myProject/settings.py
+++ _myProject/settings.py
@@ -75,14 +75,14 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = '_myProject.wsgi.application'
-"""
+
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if os.environ.get('DATABASE_URL'):
     DATABASES = {
         'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
     }
-else:"""
-DATABASES = {
+else:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
