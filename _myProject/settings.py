ace2d6311050f7e76f47b259ea8aed446270bb40
Zineb Berrachedi <berrachedizineb5@gmail.com>
2026-05-23 16:08:07 +0100

    .

--- _myProject/settings.py
+++ _myProject/settings.py
@@ -82,7 +82,7 @@ if os.environ.get('DATABASE_URL'):
         'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
     }
 else:
-    DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
